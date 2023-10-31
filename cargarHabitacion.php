816e224a8c953956a8d17b2f08d9725fb03ed7eb
Dantejx <131798272+Dantejx@users.noreply.github.com>
2023-10-31 08:56:04 -0300

    Actualizado mi pana

--- cargarHabitacion.php
+++ cargarHabitacion.php
@@ -29,23 +29,28 @@
     
 ?>
 <!DOCTYPE html>
-<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <!-- Incluye Bootstrap CSS -->
     <link rel="stylesheet" href="estilo.css">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <title>Formulario de Habitaciones</title>
+    <style>
+        body{
+        background-color: #3f363a !important;
+        }
+    </style>
 </head>
 <body>
-    <div class="cargar">
-        <form action="" method="post">
-            <h2>Nombre de la Habitacion</h2> 
-            <input type="text" name="nomHabi"> <br>
-            <h2>Cantidad de Personas</h2> 
-            <select name="cantPer" id="cantPer" >
+    <div class="container mt-5">
+        <div class="cargar">
+            <form action="" method="post">
+                <h2>Nombre de la Habitacion</h2> 
+                <input type="text" name="nomHabi" class="form-control mb-3"> 
+                <h2>Cantidad de Personas</h2> 
+                <select name="cantPer" id="cantPer" class="form-select mb-3">
                 <?php 
                 include 'conexion.php';
                 $sql1 = mysqli_query($conexion,"SELECT distinct cantidad_persona FROM servi_habitacion where 1 order by cantidad_persona asc");
@@ -57,9 +62,9 @@
                 <?php
                     }}
                 ?>
-            </select> <br>
-            <h2>Tipo</h2> 
-            <select name="tipo" id="tipo">
+             </select>
+                <h2>Tipo</h2> 
+                <select name="tipo" id="tipo" class="form-select mb-3">
                 <?php 
                 include 'conexion.php';
                 $sql2 = mysqli_query($conexion,"SELECT distinct cantidad_persona, descripcion FROM servi_habitacion where 1 ");
@@ -71,13 +76,16 @@
                 <?php
                     }}
                 ?>
-            </select> <br>
-
-            <br>
-            <button id="guardar">Guardar Habitacion</button>
-        </form>
+                            </select>
+                <br>
+                <button id="guardar" class="btn btn-dark">Guardar Habitacion</button>
+            </form>
+        </div>
     </div>
 
-    <script type="text/javascript" src="filtro.js"></script>
+    <!-- Incluye Bootstrap JS y Popper.js (requerido por Bootstrap) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@2.9.3/dist/umd/popper.min.js"></script>
 </body>
 </html>
+
